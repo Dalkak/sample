f46912782c7c263ceb09cb437b518a7f7274d55f
Gnlow <entrydark@gmail.com>
2020-01-11 09:08:16 +0900

    v0.0.1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var dalkak_1 = require("dalkak");
-exports["default"] = new dalkak_1.Pack("Sample", {
-    log: new dalkak_1.Block("log", "(text) 찍기", function (param) { return console.log(param.text); }, {
-        text: (function () { var l = new dalkak_1.Literal(dalkak_1.Type["typeof"]("string")); l.setParam("value", "안녕"); return l; })()
-    })
-});
+exports["default"] = new dalkak_1.Pack(new dalkak_1.Dict, "Sample", new dalkak_1.Dict({
+    log: new dalkak_1.Block(new dalkak_1.Dict, "log", "(text) 찍기", function (param) { return console.log(param.text); }, new dalkak_1.Dict({
+        text: dalkak_1.Literal.from("안녕")
+    }))
+}));
