2b8076979533df8a722eeda8b57c3f71779ae84c
Gnlow <entrydark@gmail.com>
2020-02-02 15:35:52 +0900

    Update dalkak

--- index.ts
+++ index.ts
@@ -2,18 +2,18 @@ import {
     Pack, Block, Literal, Type, Dict
 } from "dalkak";
 
-export default new Pack(
-    new Dict, 
-    "Sample",
-    {
-        log: new Block(
-            new Dict, 
-            "log",
-            "(text) 찍기",
-            param => console.log(param.text),
-            {
+export default new Pack({
+    name: "Sample",
+    blocks: {
+        log: new Block({
+            name: "log",
+            template: "(text) 찍기",
+            func: (param, project, platform) => {
+                console.log(param.text);
+            },
+            params: {
                 text: Literal.from("안녕")
             }
-        )
+        })
     }
-);
+});
